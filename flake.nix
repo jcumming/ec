18840b05475a1fa675a51ff7b1977170d34f174d
Jack Cummings <jack@mudshark.org>
2023-01-06 22:00:17 -0800

    build pacakges, too

--- flake.nix
+++ flake.nix
@@ -11,23 +11,34 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs { inherit system; config = { allowUnsupportedSystem = true; }; };
-      in rec {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            stdenv # gcc gnumake libc-dev curl 
-            avrdude 
-            pkgsCross.avr.buildPackages.gcc # avrgcc 
-            # pkgsCross.avr.buildPackages.libc # part of the above
-            hidapi
-            systemdMinimal
-            pkg-config
-            rustup
-            sdcc 
-            xxd
-          ];
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnsupportedSystem = true;};
         };
+        buildInputs = with pkgs; [stdenv avrdude pkgsCross.avr.buildPackages.gcc hidapi systemdMinimal pkg-config rustup sdcc xxd git];
+        mkBoard = board:
+          pkgs.stdenv.mkDerivation {
+            inherit buildInputs;
+            name = board + "-ec-firmware";
+            src = ./.;
+
+            buildPhase = ''
+              make BOARD=system76/${board} REV=${self.shortRev or "dirty"}
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp build/system76/${board}/*/ec.rom $out
+              set +x
+            '';
+          };
+      in rec {
         formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {inherit buildInputs;};
+        packages = flake-utils.lib.flattenTree {
+          oryp9-ec-firmware = mkBoard "oryp9";
+          galp5-ec-firmware = mkBoard "galp5";
+        };
       }
     );
 }
